9e514d43a696081328de84a085994773d301ff9d
vanthu12307 <vanthu12307@gmail.com>
2026-03-16 21:22:40 +0700

    up2

--- THU-PYCOBAN--1/ss16/bai_4.py
+++ THU-PYCOBAN--1/ss16/bai_4.py
@@ -1,13 +1,11 @@
 import tkinter as tk
-#chính
 root = tk.Tk()
 root.title("Calculator")
 root.geometry("300x400")
-#ô kết quả
+#KQ
 display = tk.Entry(root, font=("Arial", 20), bd=10, relief="sunken", justify="right")
 display.grid(row=0, column=0, columnspan=4, padx=10, pady=10, sticky="nsew")
 display.insert(0, "0")
-# Dsach nút
 buttons = [
     ['C', '√', 'x^y', '%'],
     ['1', '2', '3', '+'],
@@ -15,12 +13,12 @@ buttons = [
     ['7', '8', '9', '*'],
     ['0', '.', '=', '/']
 ]
-#tạo nút
+#nút
 for i, row in enumerate(buttons):
     for j, text in enumerate(row):
         btn = tk.Button(root, text=text, font=("Arial", 14), width=5, height=2)
         btn.grid(row=i+1, column=j, padx=5, pady=5, sticky="nsew")
-#dãn hàng
+#hàng
 for i in range(6):
     root.grid_rowconfigure(i, weight=1)
 for j in range(4):
